a2837330f860fa58c7ff7fe45be03dac2e077c6c
racalixta <ra.calixta@gmail.com>
2023-11-03 19:27:35 -0300

    Modulo 16 Printing possible moves

--- mod_16_chess/src/application/UI.java
+++ mod_16_chess/src/application/UI.java
@@ -53,7 +53,20 @@ public class UI {
 			System.out.print(ANSI_BLUE + (8 - i) + " " + ANSI_RESET);
 
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+
+		System.out.println(ANSI_BLUE + "  a b c d e f g h" + ANSI_RESET);
+	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print(ANSI_BLUE + (8 - i) + " " + ANSI_RESET);
+
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
@@ -61,9 +74,13 @@ public class UI {
 		System.out.println(ANSI_BLUE + "  a b c d e f g h" + ANSI_RESET);
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print(ANSI_CYAN_BACKGROUND);
+		}
+		
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
