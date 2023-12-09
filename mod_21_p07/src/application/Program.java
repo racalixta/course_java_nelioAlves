beca5f85734f4d59bde44f6ce3623d0f586e2e1b
racalixta <ra.calixta@gmail.com>
2023-12-08 22:35:58 -0300

    Modulo 21 parte 07 Insert Implementation

--- mod_21_p07/src/application/Program.java
+++ mod_21_p07/src/application/Program.java
@@ -34,7 +34,12 @@ public class Program {
 			System.out.println(obj);
 			
 		}
-
+		
+		System.out.println("\n=== TEST 4:  Seller Insert ===");	
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
 	}
 
 }
