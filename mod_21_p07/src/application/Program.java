1086f57ef8a558e18fbe2fb9a83cc349c12a3639
racalixta <ra.calixta@gmail.com>
2023-12-09 15:13:07 -0300

    Modulo 21 parte 07 Update Implementation

--- mod_21_p07/src/application/Program.java
+++ mod_21_p07/src/application/Program.java
@@ -40,6 +40,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5:  Seller Update ===");	
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 
 }
